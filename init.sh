29fc06b68b924c2cdef0cc6c731561000022d897
ben0112 <ben0112@users.noreply.github.com>
2020-09-12 01:29:25 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,4 +2,5 @@
 apt-get update && apt-get upgrade -y && apt autoremove -y
 apt-get install wget git docker-compose -y
 wget https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh -N --no-check-certificate
+bash tcp.sh
 git clone https://github.com/ben0112/TCV.git
