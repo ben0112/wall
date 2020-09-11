09b6a08013cfb700f438d23eb166fd4db44e6476
ben0112 <ben0112@users.noreply.github.com>
2020-09-11 23:23:26 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,4 @@
 apt-get update && apt-get upgrade -y && apt autoremove -y
 apt-get install wget git docker-compose -y
 wget https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh -N --no-check-certificate
-wget https://raw.githubusercontent.com/chiakge/Linux-Server-Bench-Test/master/linuxtest.sh -N --no-check-certificate
-git clone https://github.com/maidoudouo/V2ray_Trojan_Docker.git
+git clone https://github.com/ben0112/TCV.git
